2e2a1b977044c3f9f1de75ec28831705e7d78e5e
emrah <emrah.eryilmaz@nordeck.net>
2022-10-26 19:58:08 +0300

    feat: 130-network

---
+++ installer-sub-scripts/nordeck-base/130-network.sh
@@ -0,0 +1,216 @@
+#!/bin/bash
+
+# ------------------------------------------------------------------------------
+# NETWORK.SH
+# ------------------------------------------------------------------------------
+set -e
+source $INSTALLER/000-source
+
+# ------------------------------------------------------------------------------
+# ENVIRONMENT
+# ------------------------------------------------------------------------------
+MACH="nordeck-host"
+cd $MACHINES/$MACH
+
+# public interface
+DEFAULT_ROUTE=$(ip route | egrep '^default ' | head -n1)
+PUBLIC_INTERFACE=${DEFAULT_ROUTE##*dev }
+PUBLIC_INTERFACE=${PUBLIC_INTERFACE/% */}
+echo PUBLIC_INTERFACE="$PUBLIC_INTERFACE" >> $INSTALLER/000-source
+
+# IP address (IP used in the private bridge network)
+DNS_RECORD=$(grep 'address=/host/' etc/dnsmasq.d/nordeck-hosts | head -n1)
+IP=${DNS_RECORD##*/}
+echo HOST="$IP" >> $INSTALLER/000-source
+
+# remote IP address (IP used for remote connections)
+REMOTE_IP=$(ip addr show $PUBLIC_INTERFACE | ack "$PUBLIC_INTERFACE$" | \
+            xargs | cut -d " " -f2 | cut -d "/" -f1)
+echo REMOTE_IP="$REMOTE_IP" >> $INSTALLER/000-source
+
+# ------------------------------------------------------------------------------
+# INIT
+# ------------------------------------------------------------------------------
+[[ "$DONT_RUN_NETWORK_INIT" = true ]] && exit
+
+echo
+echo "------------------------ NETWORK --------------------------"
+
+# ------------------------------------------------------------------------------
+# BACKUP & STATUS
+# ------------------------------------------------------------------------------
+OLD_FILES="/root/nordeck-old-files/$DATE"
+mkdir -p $OLD_FILES
+
+# backup the files which will be changed
+[[ -f /etc/nftables.conf ]] && cp /etc/nftables.conf $OLD_FILES/
+[[ -f /etc/network/interfaces ]] && cp /etc/network/interfaces $OLD_FILES/
+[[ -f /etc/default/dnsmasq ]] && cp /etc/default/dnsmasq $OLD_FILES/
+[[ -f /etc/default/lxc-net ]] && cp /etc/default/lxc-net $OLD_FILES/
+[[ -f /etc/dnsmasq.d/nordeck-hosts ]] && \
+    cp /etc/dnsmasq.d/nordeck-hosts $OLD_FILES/
+
+# network status
+echo "# ----- ip addr -----" >> $OLD_FILES/network.status
+ip addr >> $OLD_FILES/network.status
+echo >> $OLD_FILES/network.status
+echo "# ----- ip route -----" >> $OLD_FILES/network.status
+ip route >> $OLD_FILES/network.status
+
+# nftables status
+if [[ "$(systemctl is-active nftables.service)" = "active" ]]; then
+    echo "# ----- nft list ruleset -----" >> $OLD_FILES/nftables.status
+    nft list ruleset >> $OLD_FILES/nftables.status
+fi
+
+# ------------------------------------------------------------------------------
+# PACKAGES
+# ------------------------------------------------------------------------------
+export DEBIAN_FRONTEND=noninteractive
+
+# added packages
+apt-get $APT_PROXY -y install nftables
+
+# ------------------------------------------------------------------------------
+# NETWORK CONFIG
+# ------------------------------------------------------------------------------
+# changed/added system files
+cp etc/dnsmasq.d/nordeck-hosts /etc/dnsmasq.d/
+cp etc/dnsmasq.d/nordeck-resolv /etc/dnsmasq.d/
+[[ -z "$(egrep '^DNSMASQ_EXCEPT' /etc/default/dnsmasq)" ]] && \
+    sed -i "s/^#DNSMASQ_EXCEPT/DNSMASQ_EXCEPT/" /etc/default/dnsmasq
+
+# /etc/network/interfaces
+[[ -z "$(egrep '^source-directory\s*interfaces.d' /etc/network/interfaces || true)" ]] && \
+[[ -z "$(egrep '^source-directory\s*/etc/network/interfaces.d' /etc/network/interfaces || true)" ]] && \
+[[ -z "$(egrep '^source\s*interfaces.d/\*$' /etc/network/interfaces || true)" ]] && \
+[[ -z "$(egrep '^source\s*/etc/network/interfaces.d/\*$' /etc/network/interfaces || true)" ]] && \
+[[ -z "$(egrep '^source\s*interfaces.d/\*\.cfg' /etc/network/interfaces || true)" ]] && \
+[[ -z "$(egrep '^source\s*/etc/network/interfaces.d/\*\.cfg' /etc/network/interfaces || true)" ]] && \
+[[ -z "$(egrep '^source\s*interfaces.d/nordeck-bridge.cfg' /etc/network/interfaces || true)" ]] && \
+[[ -z "$(egrep '^source\s*/etc/network/interfaces.d/nordeck-bridge.cfg' /etc/network/interfaces || true)" ]] && \
+echo -e "\nsource /etc/network/interfaces.d/nordeck-bridge.cfg" >> /etc/network/interfaces
+
+# /etc/network/cloud-interfaces-template
+if [[ -f "/etc/network/cloud-interfaces-template" ]]; then
+    [[ -z "$(egrep '^source-directory\s*interfaces.d' /etc/network/cloud-interfaces-template || true)" ]] && \
+    [[ -z "$(egrep '^source-directory\s*/etc/network/interfaces.d' /etc/network/cloud-interfaces-template || true)" ]] && \
+    [[ -z "$(egrep '^source\s*interfaces.d/\*$' /etc/network/cloud-interfaces-template || true)" ]] && \
+    [[ -z "$(egrep '^source\s*/etc/network/interfaces.d/\*$' /etc/network/cloud-interfaces-template || true)" ]] && \
+    [[ -z "$(egrep '^source\s*interfaces.d/\*\.cfg' /etc/network/cloud-interfaces-template || true)" ]] && \
+    [[ -z "$(egrep '^source\s*/etc/network/interfaces.d/\*\.cfg' /etc/network/cloud-interfaces-template || true)" ]] && \
+    [[ -z "$(egrep '^source\s*interfaces.d/nordeck-bridge.cfg' /etc/network/cloud-interfaces-template || true)" ]] && \
+    [[ -z "$(egrep '^source\s*/etc/network/interfaces.d/nordeck-bridge.cfg' /etc/network/cloud-interfaces-template || true)" ]] && \
+    echo -e "\nsource /etc/network/interfaces.d/nordeck-bridge.cfg" >> /etc/network/cloud-interfaces-template
+fi
+
+# IP forwarding
+cp etc/sysctl.d/nordeck-ip-forward.conf /etc/sysctl.d/
+sysctl -p /etc/sysctl.d/nordeck-ip-forward.conf || true
+[[ "$(cat /proc/sys/net/ipv4/ip_forward)" != 1 ]] && false
+
+# ------------------------------------------------------------------------------
+# LXC-NET
+# ------------------------------------------------------------------------------
+cp etc/default/lxc-net /etc/default/
+systemctl restart lxc-net.service
+
+# ------------------------------------------------------------------------------
+# DUMMY INTERFACE & BRIDGE
+# ------------------------------------------------------------------------------
+# the random MAC address for the dummy interface
+MAC_ADDRESS=$(date +'52:54:%d:%H:%M:%S')
+
+cp etc/network/interfaces.d/nordeck-bridge.cfg /etc/network/interfaces.d/
+sed -i "s/___MAC_ADDRESS___/${MAC_ADDRESS}/g" \
+    /etc/network/interfaces.d/nordeck-bridge.cfg
+sed -i "s/___BRIDGE___/${BRIDGE}/g" /etc/network/interfaces.d/nordeck-bridge.cfg
+cp etc/dnsmasq.d/nordeck-interface /etc/dnsmasq.d/
+sed -i "s/___BRIDGE___/${BRIDGE}/g" /etc/dnsmasq.d/nordeck-interface
+
+ifup -i /etc/network/interfaces.d/nordeck-bridge.cfg dummy0
+ifup -i /etc/network/interfaces.d/nordeck-bridge.cfg $BRIDGE
+
+# ------------------------------------------------------------------------------
+# NFTABLES
+# ------------------------------------------------------------------------------
+# recreate the custom tables
+if [[ "$RECREATE_CUSTOM_NFTABLES" = true ]]; then
+    nft delete table inet nordeck-filter 2>/dev/null || true
+    nft delete table ip nordeck-nat 2>/dev/null || true
+fi
+
+# table: nordeck-filter
+# chains: input, forward, output
+# rules: drop from the public interface to the private internal network
+nft add table inet nordeck-filter
+nft add chain inet nordeck-filter \
+    input { type filter hook input priority 0 \; }
+nft add chain inet nordeck-filter \
+    forward { type filter hook forward priority 0 \; }
+nft add chain inet nordeck-filter \
+    output { type filter hook output priority 0 \; }
+[[ -z "$(nft list chain inet nordeck-filter output | \
+ack 'ip daddr 172.22.22.0/24 drop')" ]] && \
+    nft add rule inet nordeck-filter output \
+    iif $PUBLIC_INTERFACE ip daddr 172.22.22.0/24 drop
+
+# table: nordeck-nat
+# chains: prerouting, postrouting, output, input
+# rules: masquerade
+nft add table ip nordeck-nat
+nft add chain ip nordeck-nat prerouting \
+    { type nat hook prerouting priority 0 \; }
+nft add chain ip nordeck-nat postrouting \
+    { type nat hook postrouting priority 100 \; }
+nft add chain ip nordeck-nat output \
+    { type nat hook output priority 0 \; }
+nft add chain ip nordeck-nat input \
+    { type nat hook input priority 0 \; }
+[[ -z "$(nft list chain ip nordeck-nat postrouting | \
+ack 'ip saddr 172.22.22.0/24 masquerade')" ]] && \
+    nft add rule ip nordeck-nat postrouting \
+    ip saddr 172.22.22.0/24 masquerade
+
+# table: nordeck-nat
+# chains: prerouting
+# maps: tcp2ip, tcp2port
+# rules: tcp dnat
+nft add map ip nordeck-nat tcp2ip \
+    { type inet_service : ipv4_addr \; }
+nft add map ip nordeck-nat tcp2port \
+    { type inet_service : inet_service \; }
+[[ -z "$(nft list chain ip nordeck-nat prerouting | \
+ack 'tcp dport map @tcp2ip:tcp dport map @tcp2port')" ]] && \
+    nft add rule ip nordeck-nat prerouting \
+    iif $PUBLIC_INTERFACE dnat \
+    tcp dport map @tcp2ip:tcp dport map @tcp2port
+
+# table: nordeck-nat
+# chains: prerouting
+# maps: udp2ip, udp2port
+# rules: udp dnat
+nft add map ip nordeck-nat udp2ip \
+    { type inet_service : ipv4_addr \; }
+nft add map ip nordeck-nat udp2port \
+    { type inet_service : inet_service \; }
+[[ -z "$(nft list chain ip nordeck-nat prerouting | \
+ack 'udp dport map @udp2ip:udp dport map @udp2port')" ]] && \
+    nft add rule ip nordeck-nat prerouting \
+    iif $PUBLIC_INTERFACE dnat \
+    udp dport map @udp2ip:udp dport map @udp2port
+
+# ------------------------------------------------------------------------------
+# NETWORK RELATED SERVICES
+# ------------------------------------------------------------------------------
+# dnsmasq
+systemctl stop dnsmasq.service
+systemctl start dnsmasq.service
+
+# nftables
+systemctl enable nftables.service
+
+# ------------------------------------------------------------------------------
+# STATUS
+# ------------------------------------------------------------------------------
+ip addr
