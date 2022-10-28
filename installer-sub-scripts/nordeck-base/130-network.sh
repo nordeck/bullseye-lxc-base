e51561055e8dea09ffbd43c6ff91ecb722d9b4cc
emrah <emrah.eryilmaz@nordeck.net>
2022-10-28 13:56:57 +0300

    fix: get external IP in network script, dig needed

--- installer-sub-scripts/nordeck-base/130-network.sh
+++ installer-sub-scripts/nordeck-base/130-network.sh
@@ -28,6 +28,10 @@ REMOTE_IP=$(ip addr show $PUBLIC_INTERFACE | ack "$PUBLIC_INTERFACE$" | \
             xargs | cut -d " " -f2 | cut -d "/" -f1)
 echo REMOTE_IP="$REMOTE_IP" >> $INSTALLER/000-source
 
+# external IP (Internet IP)
+EXTERNAL_IP=$(dig -4 +short myip.opendns.com a @resolver1.opendns.com) || true
+echo EXTERNAL_IP="$EXTERNAL_IP" >> $INSTALLER/000-source
+
 # ------------------------------------------------------------------------------
 # INIT
 # ------------------------------------------------------------------------------
