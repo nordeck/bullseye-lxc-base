b8a6156b6cf4d719d9b45fef7179353d4bdee9a1
emrah <emrah.eryilmaz@nordeck.net>
2022-11-24 15:59:11 +0300

    refactor: find external IP using ifconfig.me

--- installer-sub-scripts/nordeck-base/130-network.sh
+++ installer-sub-scripts/nordeck-base/130-network.sh
@@ -29,7 +29,7 @@ REMOTE_IP=$(ip addr show $PUBLIC_INTERFACE | ack "$PUBLIC_INTERFACE$" | \
 echo REMOTE_IP="$REMOTE_IP" >> $INSTALLER/000-source
 
 # external IP (Internet IP)
-EXTERNAL_IP=$(dig -4 +short myip.opendns.com a @resolver1.opendns.com) || true
+EXTERNAL_IP=$(curl -s ifconfig.me || true)
 echo EXTERNAL_IP="$EXTERNAL_IP" >> $INSTALLER/000-source
 
 # ------------------------------------------------------------------------------
